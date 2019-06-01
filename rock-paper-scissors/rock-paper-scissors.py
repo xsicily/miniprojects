ccb87580261da2a12618014c42bf1f380320576d
Sicily Wang <sicily.wang@outlook.com>
2019-06-02 00:15:13 +0300

    project_instructions word version was deleted.

--- rock-paper-scissors/rock-paper-scissors.py
+++ rock-paper-scissors/rock-paper-scissors.py
@@ -3,35 +3,50 @@
 import random
 from enum import Enum
 
-class Weapon(Enum):
-    Rock = 0
-    Paper = 1
-    Scissors = 2
+class Choice(Enum):
+    R = 1
+    P = 2
+    S = 3
+    Q = 0
 
-player_choice = 'Rock'
+raw_input = 'R'
 
-game_round = 0
+class Player():
+    def __init__(self, player_choice, weapon_r_count, weapon_p_count, weapon_s_count, weapon_used_hist):
+        self.player_choice = Choice[raw_input.upper()]
+        self.weapon_r_count = 0
+        self.weapon_p_count = 0
+        self.weapon_s_count = 0
+        self.weapon_used_hist = {}
+    def get_weapon_info(self):
+        player_choice = Choice[raw_input.upper()]
+        if player_choice == Choice.R:
+            self.weapon_used_hist[1] = self.weapon_r_count
+            self.weapon_used_hist[2] = self.weapon_p_count
+            self.weapon_used_hist[3] = self.weapon_s_count
+            self.weapon_r_count += 1
+        elif player_choice == Choice.P:
+            self.weapon_used_hist[1] = self.weapon_r_count
+            self.weapon_used_hist[2] = self.weapon_p_count
+            self.weapon_used_hist[3] = self.weapon_s_count
+            self.weapon_p_count += 1
+        elif player_choice == Choice.S:
+            self.weapon_used_hist[1] = self.weapon_r_count
+            self.weapon_used_hist[2] = self.weapon_p_count
+            self.weapon_used_hist[3] = self.weapon_s_count
+            self.weapon_s_count += 1
+        elif player_choice == Choice.Q:
+            self.weapon_used_hist[1] = self.weapon_r_count
+            self.weapon_used_hist[2] = self.weapon_p_count
+            self.weapon_used_hist[3] = self.weapon_s_count      
+        else:
+            return 'Invalid entry! Please try it again!'
+        return player_choice, self.weapon_used_hist
 
-def weapon_count(player_choice):
-    num_of_rock = 0
-    num_of_paper = 0
-    num_of_scissors = 0
-    if Weapon[player_choice] == Weapon.Rock:
-        num_of_rock = num_of_rock + 1
-        return num_of_rock
-    elif Weapon[player_choice] == Weapon.Paper:
-        num_of_paper = num_of_paper + 1
-        return num_of_paper
-    else:
-        num_of_scissors = num_of_scissors + 1
-        return num_of_scissors
+class Computer():
+    def __init__(self, comp_choice):
+        self.comp_choice = random.choice(list(Choice))
 
-comp_choice = random.choice(list(Weapon))
-
-if comp_choice == Weapon[player_choice]:
-    game_round = game_round + 1
-    print('Player and Computer tie' + ' ' + game_round + ' ' + 'round')
-elif 
 
 
 
--- rock-paper-scissors/rps.py
+++ rock-paper-scissors/rps.py
@@ -2,7 +2,80 @@
 
 import random
 import operator
-import player
+
+num_of_rock = 0
+num_of_paper = 0
+num_of_scissors = 0
+choice_of_player = {}
+
+def user(raw_input):
+    """
+    Input - string of raw_input
+    Ouput - int of player_number: 
+                            r or R - 1
+                            p or P - 2
+                            s or S - 3
+                            q or Q - 0
+            dictionary of choice_of_player:
+                            key - 0, value - how many times the player selected Rock
+                            key - 1, value - how many times the player selected Paper
+                            key - 2, value - how many times the player selected Scissors
+    """
+    global num_of_rock, num_of_paper, num_of_scissors, player_number
+    if raw_input == 'r' or raw_input == 'R':
+        player_number = 1
+        choice_of_player[1] = num_of_rock
+        choice_of_player[2] = num_of_paper
+        choice_of_player[3] = num_of_scissors
+        num_of_rock = num_of_rock + 1
+    elif raw_input == 'p' or raw_input == 'P':
+        player_number = 2
+        choice_of_player[1] = num_of_rock
+        choice_of_player[2] = num_of_paper
+        choice_of_player[3] = num_of_scissors
+        num_of_paper = num_of_paper + 1
+    elif raw_input == 's' or raw_input == 'S':
+        player_number = 3
+        choice_of_player[1] = num_of_rock
+        choice_of_player[2] = num_of_paper
+        choice_of_player[3] = num_of_scissors
+        num_of_scissors = num_of_scissors + 1 
+    elif raw_input == 'q' or raw_input == 'Q':
+        player_number = 0
+        choice_of_player[1] = num_of_rock
+        choice_of_player[2] = num_of_paper
+        choice_of_player[3] = num_of_scissors
+    else:
+        return 'Invalid entry! Please try it again!'
+    # make dictionary including all the weapon choice the player selected
+    return player_number, choice_of_player
+
+
+def computer(weapons_choice_of_user):
+    """
+    Input - dictionary of weapons choice including Rock, Paper and Scissors
+    Output - according to user's preferred weapon, to select the suitable weapon to beat user
+    """
+    global comp_number
+    # check if player has preferred choice of weapon
+    if weapons_choice_of_user[1] > weapons_choice_of_user[2] and weapons_choice_of_user[1] > weapons_choice_of_user[3]:
+        preferred_weapon = True
+    elif weapons_choice_of_user[2] > weapons_choice_of_user[1] and weapons_choice_of_user[2] > weapons_choice_of_user[3]:
+        preferred_weapon = True
+    elif weapons_choice_of_user[3] > weapons_choice_of_user[1] and weapons_choice_of_user[3] > weapons_choice_of_user[2]:
+        preferred_weapon = True
+    else:
+        preferred_weapon = False
+    # choose weapons based on preferred weapons from user
+    if True == preferred_weapon:
+        user_preferred_weapon = max(weapons_choice_of_user.items(), key=operator.itemgetter(1))[0]
+        if user_preferred_weapon == 3: 
+            comp_number = user_preferred_weapon - 2
+        else:
+            comp_number = user_preferred_weapon + 1 
+    else:
+        comp_number = random.randrange(3) + 1                    
+    return comp_number
 
 tie_round = 0
 comp_win_round = 0
@@ -10,46 +83,56 @@ user_win_round = 0
 game_round = 0
 
 
-def rps(player_number, comp_number):
+def rps(user_status):
     """
-    Input - string raw_input
+    Input - user_status including user_number and dictionary of weapons_choice_of_user
     Output - game status report
     """
     global tie_round, comp_win_round, user_win_round, game_round
-    play_quit = True
-    while not play_quit:
+    user_number = user_status[0]
+    weapons_choice_of_user = user_status[1]
+    comp_number = computer(weapons_choice_of_user)
+    if user_number != 0:
         game_round = game_round + 1
-        if user_number != 0:
-            number_diff = comp_number - user_number
-            if number_diff == 0:
-                tie_round = tie_round + 1
-                return 'Game tie!'
-            elif number_diff == -1 or number_diff == 2:
-                user_win_round = user_win_round + 1
-                return 'User win!'
-            else:
-                comp_win_round = comp_win_round + 1
-                return 'Computer wins!'
+        number_diff = comp_number - user_number
+        if number_diff == 0:
+            tie_round = tie_round + 1
+            return "Game tie!"
+        elif number_diff == -1 or number_diff == 2:
+            user_win_round = user_win_round + 1
+            return 'User win!'
         else:
-            play_quit = True
-            return 'Game Over!'
+            comp_win_round = comp_win_round + 1
+            return 'Computer wins!'
+    else:
+        return 'Total games:' + ' ' + str(game_round) + ' ' + 'rounds', \
+               'Game tie:' + ' ' + str(tie_round) + ' ' + 'rounds', \
+               'Player wins:' + ' ' + str(user_win_round) + ' ' + 'rounds', \
+               'Computer wins:' + ' ' + str(comp_win_round) + ' ' + 'rounds', \
+               'The player selected Rock:' + ' ' + str(weapons_choice_of_user[1]) + ' ' + 'times', \
+               'The player selected Paper:' + ' ' + str(weapons_choice_of_user[2]) + ' ' + 'times', \
+               'The player selected Sciccors:' + ' ' + str(weapons_choice_of_user[3]) + ' ' + 'times' 
 
 
-            show_results()
-            print('Total games:' + ' ' + str(game_round) + ' ' + 'rounds')
-            print ('Player and Computer tie:' + ' ' + str(tie_round) + ' ' + 'rounds')
-            print ('Player wins:' + ' ' + str(user_win_round) + ' ' + 'rounds')
-            print ('Computer wins:' + ' ' + str(comp_win_round) + ' ' + 'rounds')
-            print ('The number times the player selected Rock:', weapon_records_of_user[1])
-            print ('The number times the player selected Paper:', weapon_records_of_user[2])
-            print ('The number times the player selected Sciccors:', weapon_records_of_user[3])
-
+def main():
+    raw_input = 'r'
+    user_status = user(raw_input)
+    rps(user_status)
+    raw_input = 'r'
+    user_status = user(raw_input)
+    rps(user_status)
+    raw_input = 's'
+    user_status = user(raw_input)
+    rps(user_status)
+    raw_input = 'q'
+    user_status = user(raw_input)
+    game_result = rps(user_status)
+    return game_result
 
 if __name__ == "__main__":
-    raw_input = 'q'
-    user_status = player.user(raw_input)
-    user_number = user_status[0]
-    weapon_records_of_user = user_status[1]
-    comp_number = player.comp(user_status)
-    rps(user_number, comp_number)
+    print(main())
+
+
+
+
 
